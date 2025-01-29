8f116aa5db13e840b2e63e3142a736f2138acf29
MiyoshiTaishou <99778743+MiyoshiTaishou@users.noreply.github.com>
2025-01-29 15:27:25 +0900

    プレイ時SE処理実装

--- CardGame/Assets/Script/Card/AttackCardScript.cs
+++ CardGame/Assets/Script/Card/AttackCardScript.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackCardScript : CardScript
-{    
+{
     protected override void OnDropSuccess(GameObject dropTarget)
     {
         Debug.Log("攻撃カードの効果発動！対象にダメージを与える");
-
         dropTarget.GetComponent<CharStatus>().Damage(this.cardData.cardPoint);
+
+        // 一時的なオブジェクトを作成し、SE を再生
+        GameObject seObject = new GameObject("TempSE");
+        AudioSource tempAudio = seObject.AddComponent<AudioSource>();
+        tempAudio.clip = this.cardData.cardSE;
+        tempAudio.Play();
+
+        // SE の再生が終わったら削除
+        Destroy(seObject, this.cardData.cardSE.length);
+
+        // カードを即座に削除
+        Destroy(this.gameObject);
     }
+
 }
